8ff95e2e6c13fb541d0d6401f83d642c25e9a2ea
Nik Nyby <nnyby@columbia.edu>
2017-08-29 17:10:39 -0400

    Add rough sketch for econ graph model
    I'm still exploring whether I'll use wagtail, pagetree, or nothing for
    management of these graphs. In any case, this is the start of the data
    model that will be displayed with JavaScript.

--- econplayground/main/models.py
+++ econplayground/main/models.py
@@ -1,3 +1,50 @@
-# from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# define your models here
+from django.db import models
+
+
+GRAPH_CHOICES = (
+    (0, 'Demand-Supply Graph'),
+    (1, 'Labor Market'),
+    (2, 'Labor Market (perfectly inelastic)'),
+    (3, 'Cobb-Douglas'),
+    (4, 'Labor Supply'),
+    (5, 'Consumption - Saving'),
+    (6, 'Saving - Investment'),
+    (7, 'Money Market'),
+)
+
+
+class Graph(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    graph_type = models.PositiveSmallIntegerField(
+        choices=GRAPH_CHOICES, default=0)
+
+
+class Curve(models.Model):
+    # A graph has any number of curves. Most have 2.
+    graph = models.ForeignKey(Graph)
+
+    # Each Curve has a default position on its Graph. These x and
+    # y values are the offset.
+    x = models.DecimalField(max_digits=12, decimal_places=5, default=0)
+    y = models.DecimalField(max_digits=12, decimal_places=5, default=0)
+
+    label = models.TextField()
+    is_draggable = models.BooleanField(
+        default=False, help_text='Can a user drag this?')
+
+
+class Point(models.Model):
+    # Each point is connected to a Curve.
+    curve = models.ForeignKey(Curve)
+
+    # pos is the offset of this point along the single dimension of
+    # its Curve.
+    pos = models.DecimalField(max_digits=12, decimal_places=5, default=0)
+
+    label = models.TextField()
+    is_draggable = models.BooleanField(
+        default=False, help_text='Can a user drag this?')
--- econplayground/main/tests/test_models.py
+++ econplayground/main/tests/test_models.py
@@ -1,6 +1,31 @@
 from django.test import TestCase
+from econplayground.main.tests.factories import (
+    GraphFactory, CurveFactory, PointFactory
+)
 
 
-class BasicModelTest(TestCase):
-    def test_dummy(self):
-        assert True
+class GraphTest(TestCase):
+    def setUp(self):
+        self.x = GraphFactory()
+
+    def test_is_valid_from_factory(self):
+        self.x.full_clean()
+
+
+class CurveTest(TestCase):
+    def setUp(self):
+        graph = GraphFactory()
+        self.x = CurveFactory(graph=graph)
+
+    def test_is_valid_from_factory(self):
+        self.x.full_clean()
+
+
+class PointTest(TestCase):
+    def setUp(self):
+        graph = GraphFactory()
+        curve = CurveFactory(graph=graph)
+        self.x = PointFactory(curve=curve)
+
+    def test_is_valid_from_factory(self):
+        self.x.full_clean()
