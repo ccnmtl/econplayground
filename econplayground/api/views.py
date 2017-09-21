d8a9b9fd6d154d056752f7e47105d03049b43229
Nik Nyby <nnyby@columbia.edu>
2017-09-21 15:38:20 -0400

    A few API fixes for connecting to econplayground.js

--- econplayground/api/views.py
+++ econplayground/api/views.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from econplayground.main.models import Graph
 from econplayground.api.serializers import GraphSerializer
 
@@ -10,4 +9,3 @@ from econplayground.api.serializers import GraphSerializer
 class GraphViewSet(viewsets.ModelViewSet):
     queryset = Graph.objects.all()
     serializer_class = GraphSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
