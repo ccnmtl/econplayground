432a12c15ba9d86549f4bcf8679a3bfed77ddd6e
Nik Nyby <nnyby@columbia.edu>
2017-10-14 10:59:36 -0400

    Add embed view basics - PMT #112696

--- econplayground/main/tests/test_views.py
+++ econplayground/main/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
+from econplayground.main.tests.factories import GraphFactory
+from econplayground.main.tests.mixins import LoggedInTestMixin
 
 
 class BasicTest(TestCase):
@@ -14,3 +16,11 @@ class BasicTest(TestCase):
         response = self.c.get("/smoketest/")
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "PASS")
+
+
+class EmbedViewTest(LoggedInTestMixin, TestCase):
+    def test_get(self):
+        g = GraphFactory()
+        r = self.client.get('/graph/{}/embed/'.format(g.pk))
+        self.assertEqual(r.status_code, 200)
+        self.assertContains(r, g.title)
