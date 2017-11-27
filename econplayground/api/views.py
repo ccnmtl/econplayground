3a801f35c8576527aff600e561c88d9d2e5fd94a
Nik Nyby <nnyby@columbia.edu>
2017-11-27 12:20:17 -0500

    Add test for making a duplicate submission

--- econplayground/api/views.py
+++ econplayground/api/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from econplayground.main.models import Graph, Submission
 from econplayground.api.serializers import (
     GraphSerializer, SubmissionSerializer
@@ -22,4 +23,9 @@ class SubmissionViewSet(viewsets.ModelViewSet):
         return Submission.objects.filter(user=user)
 
     def perform_create(self, serializer):
+        if Submission.objects.filter(
+                graph=serializer.validated_data.get('graph'),
+                user=self.request.user).exists():
+            raise ValidationError('Submission exists')
+
         serializer.save(user=self.request.user)
