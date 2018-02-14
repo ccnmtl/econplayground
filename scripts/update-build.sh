53278d77d8866d7d8b342e00bee0a1a5ef2ce34f
Nik Nyby <nnyby@columbia.edu>
2018-02-14 12:43:04 -0500

    rename js build

--- scripts/update-build.sh
+++ scripts/update-build.sh
@@ -6,5 +6,5 @@
 DEST_DIR="media/econplayground.js/"
 ORIG_DIR="$HOME/public_html/econplayground.js/build/"
 
-cp $ORIG_DIR/main.js $DEST_DIR/main-a.js
-cp $ORIG_DIR/viewer.js $DEST_DIR/viewer-a.js
+cp $ORIG_DIR/main.js $DEST_DIR/main-b.js
+cp $ORIG_DIR/viewer.js $DEST_DIR/viewer-b.js
