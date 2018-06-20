47615760765b448c6779f9ee614f7f9e883d621e
Nik Nyby <nnyby@columbia.edu>
2018-06-20 14:11:19 -0400

    Only allow one Assessment per Graph

--- econplayground/main/tests/test_models.py
+++ econplayground/main/tests/test_models.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from econplayground.main.models import Assessment
 from econplayground.main.tests.factories import (
     GraphFactory, JXGLineFactory, JXGLineTransformationFactory,
     SubmissionFactory,
@@ -44,6 +46,11 @@ class AssessmentTest(TestCase):
 
     def test_is_valid_from_factory(self):
         self.x.full_clean()
+
+    def test_only_one_assessment_per_graph(self):
+        self.assertEqual(Assessment.objects.count(), 1)
+        with self.assertRaises(IntegrityError):
+            AssessmentFactory(graph=self.x.graph)
 
 
 class AssessmentRuleTest(TestCase):
