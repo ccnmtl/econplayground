dd93f0d5b3a5b90566af9f8f1f60497a69326b2a
Nikolas Nyby <nikolas@gnu.org>
2023-07-17 14:45:25 -0400

    Allow question to be removed from steps
    Add question removal from steps + a few unit tests for this.

--- econplayground/assignment/tests/test_views.py
+++ econplayground/assignment/tests/test_views.py
@@ -14,6 +14,41 @@ class AssignmentManagementViewTest(LoggedInTestInstructorMixin, TestCase):
         super().setUp()
         self.assignment = AssignmentFactory()
 
+    def test_question_management(self):
+        r = self.client.post(
+            reverse('tree_edit', kwargs={'pk': self.assignment.pk}), {
+                'action': 'add_step',
+            }, follow=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertContains(r, 'New step added.')
+        self.assertEqual(Step.objects.count(), 2)
+        step = Step.objects.last()
+        q = QuestionFactory()
+
+        # Save question
+        r = self.client.post(
+            reverse('tree_edit', kwargs={'pk': self.assignment.pk}), {
+                'action': 'save',
+                'step_question_' + str(step.pk): q.pk,
+            }, follow=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertContains(r, 'Questions updated.')
+        self.assertEqual(Step.objects.count(), 2)
+        step.refresh_from_db()
+        self.assertEqual(step.question, q)
+
+        # Remove question
+        r = self.client.post(
+            reverse('tree_edit', kwargs={'pk': self.assignment.pk}), {
+                'action': 'save',
+                'step_question_' + str(step.pk): 0,
+            }, follow=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertContains(r, 'Questions updated.')
+        self.assertEqual(Step.objects.count(), 2)
+        step.refresh_from_db()
+        self.assertEqual(step.question, None)
+
     def test_build_linear_assignment(self):
         r = self.client.post(
             reverse('tree_edit', kwargs={'pk': self.assignment.pk}), {
@@ -55,8 +90,9 @@ class AssignmentManagementViewTest(LoggedInTestInstructorMixin, TestCase):
         # self.assertEqual(Question.objects.count(), 1)
 
         q = QuestionFactory()
+        q2 = QuestionFactory()
 
-        # Save a question
+        # Save question
         r = self.client.post(
             reverse('tree_edit', kwargs={'pk': self.assignment.pk}), {
                 'action': 'save',
@@ -65,6 +101,16 @@ class AssignmentManagementViewTest(LoggedInTestInstructorMixin, TestCase):
         self.assertEqual(r.status_code, 200)
         self.assertContains(r, 'Questions updated.')
         self.assertEqual(Step.objects.count(), 5)
+        self.assertEqual(Step.objects.first().question, q)
+
+        r = self.client.post(
+            reverse('tree_edit', kwargs={'pk': self.assignment.pk}), {
+                'action': 'save',
+                'step_question_' + str(Step.objects.all()[1].pk): q2.pk,
+            }, follow=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertContains(r, 'Questions updated.')
+        self.assertEqual(Step.objects.all()[1].question, q2)
 
 
 class AssignmentStudentFlowViewTest(LoggedInTestStudentMixin, TestCase):
