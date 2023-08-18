b35c3189de4b9899777fc1184c0748b4d5d1e096
Nikolas Nyby <nikolas@gnu.org>
2023-08-18 09:35:00 -0400

    Update docker.mk

--- docker.mk
+++ docker.mk
@@ -1,4 +1,17 @@
-docker-shell:
-	docker compose run web python manage.py shell_plus --settings=econplayground.settings_docker
+# VERSION=2.0.0
+# Docker related stuff
 
-.PHONY: docker-shell
+# Start up docker dev server with docker-compose
+docker:
+	docker compose up
+
+# Enter a command-line shell in the web container
+docker-shell:
+	docker compose run web bash
+
+# Enter an interactive django shell
+docker-django-shell:
+	docker compose run web python manage.py shell_plus \
+		--settings=$(APP).settings_docker
+
+.PHONY: docker docker-shell docker-django-shell
