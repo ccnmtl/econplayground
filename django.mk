35371c9542dcc007247e4519745b4e903b47cacf
Nikolas Nyby <nikolas@gnu.org>
2023-11-03 10:43:55 -0400

    Update makefiles

--- django.mk
+++ django.mk
@@ -23,8 +23,8 @@ MANAGE ?= ./manage.py
 REQUIREMENTS ?= requirements.txt
 SYS_PYTHON ?= python3
 PY_SENTINAL ?= $(VE)/sentinal
-WHEEL_VERSION ?= 0.40.0
-PIP_VERSION ?= 23.1.2
+WHEEL_VERSION ?= 0.41.2
+PIP_VERSION ?= 23.2.1
 MAX_COMPLEXITY ?= 10
 INTERFACE ?= localhost
 RUNSERVER_PORT ?= 8000
@@ -34,7 +34,7 @@ FLAKE8 ?= $(VE)/bin/flake8
 PIP ?= $(VE)/bin/pip
 COVERAGE ?= $(VE)/bin/coverage
 
-jenkins: check flake8 test bandit eslint
+jenkins: check flake8 test bandit
 
 $(PY_SENTINAL): $(REQUIREMENTS)
 	rm -rf $(VE)
@@ -76,29 +76,7 @@ clean:
 	rm -rf reports
 	rm -f celerybeat-schedule
 	rm -f .coverage
-	rm -rf node_modules build
+	rm -rf node_modules
 	find . -name '*.pyc' -exec rm {} \;
 
-pull:
-	git pull
-	make check
-	make test
-	make migrate
-	make flake8
-
-rebase:
-	git pull --rebase
-	make check
-	make test
-	make migrate
-	make flake8
-
-# run this one the very first time you check
-# this out on a new machine to set up dev
-# database, etc. You probably *DON'T* want
-# to run it after that, though.
-install: jenkins
-	createdb $(APP)
-	make migrate
-
-.PHONY: jenkins test flake8 runserver migrate check shell clean pull rebase install
+.PHONY: jenkins test flake8 runserver migrate check shell clean
