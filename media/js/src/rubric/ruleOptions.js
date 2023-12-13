a6603e7bcf3dcae47fa7166025ca31e614d47ad7
Evan Petersen <emp2221@columbia.edu>
2023-12-13 16:31:08 -0500

    Filled out the rubric assessments for the remaining graphTypes

--- media/js/src/rubric/ruleOptions.js
+++ media/js/src/rubric/ruleOptions.js
@@ -211,7 +211,84 @@ const consumptionSavingConstraintOptions = [
 ];
 
 // Type 8
-const linearDemandSupplyThreeFunctionsOptions = [];
+const linearDemandSupplyThreeFunctionsOptions = [
+    {
+        name: 'Orange line',
+        value: 'line1',
+    },
+    {
+        name: 'Blue line',
+        value: 'line2',
+    },
+    {
+        name: 'Red line',
+        value: 'line3',
+    },
+    {
+        name: 'Orange line label',
+        value: 'line_1_label'
+    },
+    {
+        name: 'Blue line label',
+        value: 'line_2_label'
+    },
+    {
+        name: 'X-axis label',
+        value: 'x_axis_label'
+    },
+    {
+        name: 'Y-axis label',
+        value: 'y_axis_label'
+    },
+    {
+        name: 'Orange-Blue Intersection label',
+        value: 'intersection_1_label'
+    },
+    {
+        name: 'Orange-Blue Intersection\'s horizontal line label',
+        value: 'intersection_1_horiz_line_label'
+    },
+    {
+        name: 'Orange-Blue Intersection\'s vertical line label',
+        value: 'intersection_1_vert_line_label'
+    },
+    {
+        name: 'Blue-Red Intersection point label',
+        value: 'intersection_2_label'
+    },
+    {
+        name: 'Blue-Red Intersection\'s horizontal line label',
+        value: 'intersection_2_horiz_line_label'
+    },
+    {
+        name: 'Blue-Red Intersection\'s vertical line label',
+        value: 'intersection_2_vert_line_label'
+    },
+    {
+        name: 'Orange-Red Intersection point label',
+        value: 'intersection_3_label'
+    },
+    {
+        name: 'Orange-Red Intersection\'s horizontal line label',
+        value: 'intersection_3_horiz_line_label'
+    },
+    {
+        name: 'Orange-Red Intersection\'s vertical line label',
+        value: 'intersection_3_vert_line_label'
+    },
+    {
+        name: 'Orange line slope',
+        value: 'line_1_slope'
+    },
+    {
+        name: 'Blue line slope',
+        value: 'line_2_slope'
+    },
+    {
+        name: 'Red line slope',
+        value: 'line_3_slope'
+    },
+];
 
 // Graph type 9
 // Linear Demand and Supply: Areas
@@ -347,19 +424,308 @@ const inputMarketsAreasOptions = [
 ];
 
 // Type 11
-const consumptionSavingOptimalChoiceOptions = [];
+const consumptionSavingOptimalChoiceOptions = [
+    {
+        name: 'y1',
+        value: 'a1'
+    },
+    {
+        name: 'y2',
+        value: 'a2'
+    },
+    {
+        name: 'W',
+        value: 'a3'
+    },
+    {
+        name: 'r',
+        value: 'a4'
+    },
+    {
+        name: 'β (beta)',
+        value: 'a5'
+    },
+    {
+        name: 'Orange line label',
+        value: 'line_1_label'
+    },
+    {
+        name: 'Blue line label',
+        value: 'line_2_label'
+    },
+    {
+        name: 'Endowment point label',
+        value: 'intersection_label'
+    },
+    {
+        name: 'Endowment point\'s horizontal line label',
+        value: 'intersection_horiz_line_label'
+    },
+    {
+        name: 'Endowment point\'s vertical line label',
+        value: 'intersection_vert_line_label'
+    },
+    {
+        name: 'Optimal point label',
+        value: 'intersection_2_label'
+    },
+    {
+        name: 'Optimal point\'s horizontal line label',
+        value: 'intersection_2_horiz_line_label'
+    },
+    {
+        name: 'Optimal point\'s vertical line label',
+        value: 'intersection_2_vert_line_label'
+    },
+    {
+        name: 'Orange point\'s horizontal line label',
+        value: 'intersection_3_horiz_line_label'
+    },
+    {
+        name: 'Orange point\'s vertical line label',
+        value: 'intersection_3_vert_line_label'
+    },
+];
 
 // Type 15
-const consumptionLeisureOptimalChoiceOptions = [];
+const consumptionLeisureOptimalChoiceOptions = [
+    {
+        name: 'Horizontal intercept value: T',
+        value: 'a1'
+    },
+    {
+        name: 'Real Wage: w',
+        value: 'a2'
+    },
+    {
+        name: 'Rel. Preference: α',
+        value: 'a3'
+    },
+    {
+        name: 'Tax Rate: t',
+        value: 'a4'
+    },
+    {
+        name: 'Budget line label',
+        value: 'line_1_label'
+    },
+    {
+        name: 'X-axis label',
+        value: 'x_axis_label'
+    },
+    {
+        name: 'Y-axis label',
+        value: 'y_axis_label'
+    },
+    {
+        name: 'Intersection\'s horizontal line label',
+        value: 'intersection_horiz_line_label'
+    },
+    {
+        name: 'Intersection\'s vertical line label',
+        value: 'intersection_vert_line_label'
+    },
+    {
+        name: 'Optimal point label',
+        value: 'intersection_label'
+    },
+    {
+        name: 'Optimal point\'s horizontal line label',
+        value: 'intersection_horiz_line_label'
+    },
+    {
+        name: 'Optimal point\'s vertical line label',
+        value: 'intersection_vert_line_label'
+    }
+];
 
 // Type 12
-const inputOutputIllustrationsOptions = [];
+const inputOutputIllustrationsOptions = [
+    {
+        name: 'Cobb-Doubglas A',
+        value: 'cobb_douglas_a'
+    },
+    {
+        name: 'Cobb-Doubglas K',
+        value: 'cobb_douglas_k'
+    },
+    {
+        name: 'Cobb-Doubglas Alpha',
+        value: 'cobb_douglas_alpha'
+    },
+    {
+        name: 'Cobb-Doubglas L',
+        value: 'cobb_douglas_l'
+    },
+    {
+        name: 'Orange line slope',
+        value: 'line_1_slope'
+    },
+    {
+        name: 'Orange line label',
+        value: 'line_1_label'
+    },
+    {
+        name: 'Blue line label',
+        value: 'line_2_label'
+    },
+    {
+        name: 'Intersection point label',
+        value: 'intersection_label'
+    },
+    {
+        name: 'Intersection\'s horizontal line label',
+        value: 'intersection_horiz_line_label'
+    },
+    {
+        name: 'Intersection\'s vertical line label',
+        value: 'intersection_vert_line_label'
+    },
+];
 
 // Type 13
-const linearDemandSupplyTwoDiagramsOptions = [];
+const linearDemandSupplyTwoDiagramsOptions = [
+    {
+        name: 'Orange line',
+        value: 'line1',
+    },
+    {
+        name: 'Blue line',
+        value: 'line2',
+    },
+    {
+        name: 'Orange line label',
+        value: 'line_1_label'
+    },
+    {
+        name: 'Blue line label',
+        value: 'line_2_label'
+    },
+    {
+        name: 'X-axis label',
+        value: 'x_axis_label'
+    },
+    {
+        name: 'Y-axis label',
+        value: 'y_axis_label'
+    },
+    {
+        name: 'Intersection point label',
+        value: 'intersection_label'
+    },
+    {
+        name: 'Intersection\'s horizontal line label',
+        value: 'intersection_horiz_line_label'
+    },
+    {
+        name: 'Intersection\'s vertical line label',
+        value: 'intersection_vert_line_label'
+    },
+    {
+        name: 'Orange line slope',
+        value: 'line_1_slope'
+    },
+    {
+        name: 'Blue line slope',
+        value: 'line_2_slope'
+    },
+    {
+        name: 'Orange line 2',
+        value: 'line3',
+    },
+    {
+        name: 'Blue line 2',
+        value: 'line4',
+    },
+    {
+        name: 'Orange line label',
+        value: 'line_3_label'
+    },
+    {
+        name: 'Blue line label',
+        value: 'line_4_label'
+    },
+    {
+        name: 'X-axis label',
+        value: 'x_axis_2_label'
+    },
+    {
+        name: 'Y-axis label',
+        value: 'y_axis_2_label'
+    },
+    {
+        name: 'Intersection point label',
+        value: 'intersection_2_label'
+    },
+    {
+        name: 'Intersection\'s horizontal line label',
+        value: 'intersection_2_horiz_line_label'
+    },
+    {
+        name: 'Intersection\'s vertical line label',
+        value: 'intersection_2_vert_line_label'
+    },
+    {
+        name: 'Orange line slope',
+        value: 'line_3_slope'
+    },
+    {
+        name: 'Blue line slope',
+        value: 'line_4_slope'
+    },
+];
 
 // Type 14
-const inputMarketsTwoDiagramsOptions = [];
+const inputMarketsTwoDiagramsOptions = [
+    {
+        name: 'Orange line',
+        value: 'line1',
+    },
+    {
+        name: 'Blue line',
+        value: 'line2',
+    },
+    {
+        name: 'Orange line 2',
+        value: 'line3',
+    },
+    {
+        name: 'Blue line 2',
+        value: 'line4',
+    },
+    {
+        name: 'Orange line slope',
+        value: 'line_1_slope'
+    },
+    {
+        name: 'Cobb-Doubglas A',
+        value: 'cobb_douglas_a'
+    },
+    {
+        name: 'Cobb-Doubglas K',
+        value: 'cobb_douglas_k'
+    },
+    {
+        name: 'Orange line label',
+        value: 'line_1_label'
+    },
+    {
+        name: 'Blue line label',
+        value: 'line_2_label'
+    },
+    {
+        name: 'Intersection point label',
+        value: 'intersection_label'
+    },
+    {
+        name: 'Intersection\'s horizontal line label',
+        value: 'intersection_horiz_line_label'
+    },
+    {
+        name: 'Intersection\'s vertical line label',
+        value: 'intersection_vert_line_label'
+    },
+];
 
 /**
  * getRuleOptions
@@ -367,6 +733,7 @@ const inputMarketsTwoDiagramsOptions = [];
  * Get the appropriate rule options based on graph type.
  */
 const getRuleOptions = (graphType) => {
+    console.log('graphType', graphType);
     switch (graphType) {
         case 0:
             return linearDemandOptions;
