028fbf386945bb5a85a0b264c1fd005bd89dc389
Evan Petersen <emp2221@columbia.edu>
2024-06-10 16:28:55 -0400

    Added Function components to all graphs

--- media/js/src/editors/CostFunctionsTotalEditor.jsx
+++ media/js/src/editors/CostFunctionsTotalEditor.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RangeEditor from '../form-components/RangeEditor.js';
 import { handleFormUpdate } from '../utils.js';
+import { MathJax } from 'better-react-mathjax';
 
 export default class CostFunctionsTotalEditor extends React.Component {
     render() {
         return (
             <div>
+                <h2>Function</h2>
+                <div className="col">
+                    <MathJax>
+                        {`$$Cost=a+bx+cx^2=
+                            ${this.props.gA1}+
+                            ${this.props.gA2}x+
+                            ${this.props.gA3}x^2$$`}
+                    </MathJax>
+                    <MathJax>
+                        {`$$F_{cost}=a=${this.props.gA1}$$`}
+                    </MathJax>
+                    <MathJax>
+                        {'$$V_{cost}=Cost - F_{cost}$$'}
+                    </MathJax>
+                </div>
+                <hr />
                 {this.props.displaySliders && (
                     <React.Fragment>
                         <RangeEditor
--- media/js/src/editors/CostFunctionsUnitEditor.jsx
+++ media/js/src/editors/CostFunctionsUnitEditor.jsx
@@ -3,12 +3,29 @@ import PropTypes from 'prop-types';
 import RangeEditor from '../form-components/RangeEditor.js';
 import DefineRange from '../form-components/DefineRange.jsx';
 import { handleFormUpdate } from '../utils.js';
+import { MathJax } from 'better-react-mathjax';
 
 
 export default class CostFunctionsUnitEditor extends React.Component {
     render() {
         return (
             <div>
+                <h2>Function</h2>
+                <div className="col">
+                    <MathJax>
+                        {`$$Cost=a+bx+cx^2=
+                            ${this.props.gA1}+
+                            ${this.props.gA2}x+
+                            ${this.props.gA3}x^2$$`}
+                    </MathJax>
+                    <MathJax>
+                        {`$$F_{cost}=a=${this.props.gA1}$$`}
+                    </MathJax>
+                    <MathJax>
+                        {'$$V_{cost}=Cost - F_{cost}$$'}
+                    </MathJax>
+                </div>
+                <hr />
                 {this.props.isInstructor && this.props.isEditing && (
                     <div className="row">
                         {[ // [dataId, label, noMin?]
