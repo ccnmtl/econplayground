08e230a1a94c5cf1662288e4ba38f19955fd59b0
Nikolas Nyby <nikolas@gnu.org>
2024-08-08 17:45:07 -0400

    Add missing Cobb Douglas variable names + refactor defaults system

---
+++ media/js/src/graphUtils.js
@@ -0,0 +1,115 @@
+import {
+    defaults as optimalChoiceConsumptionDefaults,
+    untoggledDefaults as untoggledOptimalChoiceConsumptionDefaults
+} from './graphs/OptimalChoiceConsumption.js';
+import {
+    defaults as costFunctionsDefaults
+} from './graphs/CostFunctionsTotalGraph.js';
+import {
+    defaults as costMinimizingDefaults
+} from './graphs/OptimalChoiceCostMinimizing.js';
+import {
+    defaults as cobbDouglasDefaults
+} from './graphs/CobbDouglasGraph.js';
+
+/**
+ * Set defaults to the given state update object based on toggle and
+ * function choice.
+ *
+ * Returns an object.
+ */
+const setDefaults = function(
+    updateObj, defaults, untoggledDefaults, functionChoice
+) {
+    if (Object.hasOwn(updateObj, 'gFunctionChoice')) {
+        Object.assign(
+            updateObj,
+            defaults[updateObj.gFunctionChoice]);
+    }
+
+    if (Object.hasOwn(updateObj, 'gToggle')) {
+        if (updateObj.gToggle) {
+            Object.assign(
+                updateObj,
+                defaults[functionChoice]);
+        } else {
+            Object.assign(updateObj, untoggledDefaults);
+        }
+    }
+
+    return updateObj;
+};
+
+/**
+ * Given a graph type and a state update object,
+ * modify the state based on if it contains alterations
+ * to properties such as gFunctionChoice and gToggle.
+ *
+ * Returns an object.
+ */
+export const setDynamicGraphDefaults = function(state, updateObj) {
+    if (state.gType === 17) {
+        updateObj = setDefaults(
+            updateObj, optimalChoiceConsumptionDefaults,
+            untoggledOptimalChoiceConsumptionDefaults,
+            state.gFunctionChoice);
+    } else if (state.gType === 18) {
+        updateObj = setDefaults(
+            updateObj, costFunctionsDefaults,
+            costFunctionsDefaults,
+            state.gFunctionChoice);
+    } else if (state.gType === 21) {
+        updateObj = setDefaults(
+            updateObj,
+            costMinimizingDefaults,
+            {gXAxisMax: 1000, gYAxisMax: 1000},
+            state.gFunctionChoice);
+    }
+
+    return updateObj;
+};
+
+/**
+ * Get the initial/default state for a graph type.
+ *
+ * Returns an object.
+ */
+export const getDefaultGraphState = function(graphType, state) {
+    // Specific defaults based on graph type.
+
+    if (graphType == 3) {
+        Object.assign(state, cobbDouglasDefaults);
+    } else if (graphType === 15) {
+        state.gA4 = 0.5;
+    } else if (graphType === 17) {
+        Object.assign(
+            state, untoggledOptimalChoiceConsumptionDefaults);
+    } else if (graphType === 18) {
+        Object.assign(state, costFunctionsDefaults[0]);
+    } else if (graphType === 21) {
+        Object.assign(state, {
+            gA1: 5,
+            gA2: 10,
+            gA3: 2500,
+            gA4: 0.5,
+            gA5: 0.5,
+            gA6: 0,
+            gA7: 0,
+            gA8: 0,
+            gXAxisMax: 1000,
+            gYAxisMax: 1000
+        });
+    } else if (graphType === 23) {
+        Object.assign(state, {
+            gA1: 1500,
+            gA2: 100,
+            gA3: 0,
+            gLine1Slope: 2,
+            gLine2Slope: 2,
+            gXAxisMax: 1000,
+            gYAxisMax: 2500
+        });
+    }
+
+    return state;
+};
