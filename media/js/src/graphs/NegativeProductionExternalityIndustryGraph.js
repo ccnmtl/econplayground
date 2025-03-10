bfd83bdd1ecf5e8dbee17dc1e953264b42246aaf
Nikolas Nyby <nnyby@columbia.edu>
2025-03-10 12:59:23 -0400

    Add Externalities graphPane() info

--- media/js/src/graphs/NegativeProductionExternalityIndustryGraph.js
+++ media/js/src/graphs/NegativeProductionExternalityIndustryGraph.js
@@ -1,6 +1,30 @@
 import {Graph} from './Graph.js';
 
 export class NegativeProductionExternalityIndustryGraph extends Graph {
+    static getGraphPane(gFunctionChoice) {
+        return [
+            {
+                label: 'Unregulated Output Q*',
+                color: 'red',
+                value: 400
+            },
+            {
+                label: 'Unregulated Price P*',
+                color: 'red',
+                value: 100
+            },
+            {
+                label: 'Socially Desirable Output q<sup>soc</sup>',
+                color: 'orange',
+                value: 300
+            },
+            {
+                label: 'Socially Desirable Price P<sup>soc</sup>',
+                color: 'orange',
+                value: 112.5
+            },
+        ];
+    }
 }
 
 export const mkNegativeProductionExternalityIndustry = function(board, options) {
--- media/js/src/graphs/PositiveExternalityIndustryGraph.js
+++ media/js/src/graphs/PositiveExternalityIndustryGraph.js
@@ -1,6 +1,30 @@
 import {Graph} from './Graph.js';
 
 export class PositiveExternalityIndustryGraph extends Graph {
+    static getGraphPane(gFunctionChoice) {
+        return [
+            {
+                label: 'Unregulated Output Q*',
+                color: 'red',
+                value: 240
+            },
+            {
+                label: 'Unregulated Price P*',
+                color: 'red',
+                value: 120
+            },
+            {
+                label: 'Socially Desirable Output q<sup>soc</sup>',
+                color: 'orange',
+                value: 266.667
+            },
+            {
+                label: 'Socially Desirable Price P<sup>soc</sup>',
+                color: 'orange',
+                value: 133.333
+            },
+        ];
+    }
 }
 
 export const mkPositiveExternalityIndustry = function(board, options) {
